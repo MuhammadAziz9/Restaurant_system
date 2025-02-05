8a2e49cb7c3f3ae03701eb88e5c9e317d80b9eb2
A1909bek <aa2004bek@gmail.com>
2025-02-05 20:03:01 +0000

    next commit

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home,menuitem_detail
 
 urlpatterns = [
-    path('',home,name='home')
+    path('',home,name='home'),
+    path('menuitem/<int:id>/',menuitem_detail,name='menuitem_detail'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import MenuItem,Category
 
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html')
+    menuitems = MenuItem.objects.all()
+    context = {
+        'menuitems':menuitems
+    }
+    return render(request,'home.html',context=context)
+
+def menuitem_detail(request,id):
+    menuitem = get_object_or_404(MenuItem,id=id)
+    context = {
+        'menuitem':menuitem
+    }
+    return render(request,'menuitem_detail.html',context)
+
+
+
 
