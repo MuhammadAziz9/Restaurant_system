0c8e4087ffae8abbfacda6dc1bd75302a972b9c8
A1909bek <aa2004bek@gmail.com>
2025-02-06 14:02:33 +0000

    next commit

---
+++ order/forms.py
@@ -0,0 +1,6 @@
+# from django import forms
+# from .models import Order
+# # Create your tests here.
+# class OrderForm(forms.ModelForm):
+#     class Meta:
+#         model = Order
--- order/views.py
+++ order/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+# views.py
+# from django.shortcuts import render, redirect
+# from .models import Order, OrderItem
+# from .forms import OrderForm
+# from menu.models import MenuItem
 
-# Create your views here.
+# def create_order(request):
+#     if request.method == 'POST':
+#         form = OrderForm(request.POST)
+#         if form.is_valid():
+#             order = form.save(commit=False)
+#             order.customer = request.user  # Foydalanuvchi kimligini olish
+#             order.save()
+            
+#             # Buyurtma elementlarini qo'shish (order_item larni yaratish)
+#             order_items = request.POST.getlist('items')  # misol uchun, buyurtmalarni olish
+#             total_price = 0
+#             for item_id in order_items:
+#                 item = MenuItem.objects.get(id=item_id)
+#                 order_item = OrderItem(order=order, menu_item=item, quantity=1, price=item.price)
+#                 order_item.save()
+#                 total_price += item.price  # Totalni hisoblash
+            
+#             order.total_price = total_price
+#             order.save()
+            
+#             return redirect('order_success')  # muvaffaqiyatli buyurtma sahifasiga yo'naltirish
+#     else:
+#         form = OrderForm()
+#     return render(request, 'order/create_order.html', {'form': form})
+
